b8f14de53b94e937ccebd9b63372a7d07fd679ef
Ahia Kleine Chris <wickeddeveloper4l@gmail.com>
2023-07-06 14:47:43 +0100

    created a middleware that logs the time taken to get a response from the server

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ const friends = [
   },
 ];
 
+app.use((req, res, next) => {
+  const start = Date.now();
+
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Welcome to Chris Express Server!");
 });
@@ -27,6 +36,21 @@ app.get("/friends", (req, res) => {
   res.json(friends);
 });
 
+app.post("/friends", (req, res) => {
+  const newFriend = {
+    name: req.body.name,
+    id: friends.length,
+  };
+
+  if (!req.body.name) {
+    return res.status(400).json({
+      error: "unable to create friend because there's no name value!",
+    });
+  }
+
+  friends.push(newFriend);
+  res.json(newFriend);
+});
 app.get("/friends/:friendId", (req, res) => {
   const friendId = Number(req.params.friendId);
   const friend = friends[friendId];
