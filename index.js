fa86af7afc0b52a1913a8115e9297d5204f8c3cf
Ahia Kleine Chris <wickeddeveloper4l@gmail.com>
2023-07-06 14:36:14 +0100

    created a friends endpoint with the express server

--- index.js
+++ index.js
@@ -4,6 +4,41 @@ const app = express();
 
 const PORT = 8080;
 
+const friends = [
+  {
+    name: "Chris",
+    id: "0",
+  },
+  {
+    name: "seyi",
+    id: "1",
+  },
+  {
+    name: "kleine",
+    id: "2",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Chris Express Server!");
+});
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+
+  if (friend) {
+    res.json(friend);
+  } else {
+    res.status(400).json({
+      error: "No friend found",
+    });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Listenoing to chris express server at ${PORT}`);
 });
